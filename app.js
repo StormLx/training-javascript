9264776d4c7772b92c599b011b188311fbfac136
Alexandre Caldato <caldato.alexandre@gmail.com>
2022-02-10 23:56:01 +0100

    added new functionality to create articles

--- app.js
+++ app.js
@@ -1,30 +1,78 @@
 // ELEMENTS
-const inputSearch = document.getElementById('input--search');
-const btnSearch = document.getElementById('btn--search');
+let inputSearch;
+let btnSearch;
+let cardResult;
+let btnCreateArticle;
+let inputCreateArticleTitle;
+let inputCreateArticleDescription;
+let btnArticleDelete;
+const btnNavHome = document.getElementById('btn--nav--home');
+const btnNavSearch = document.getElementById('btn--nav--search');
+const btnNavCreateArticle = document.getElementById('btn--nav--create-article');
 const app = document.getElementById('app');
 
+
 // DATA
-const articles = [{
-    title: 'Macron en prison',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore error distinctio doloremque inventore eligendi atque ad dolor pariatur illum eos, nemo perferendis quos vitae alias modi porro assumenda tempora rerum!'
-},
+const articles = [
     {
+        id: 1,
+        title: 'Macron en prison',
+        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore error distinctio doloremque inventore eligendi atque ad dolor pariatur illum eos, nemo perferendis quos vitae alias modi porro assumenda tempora rerum!'
+    },
+    {
+        id: 2,
         title: 'Mbappé au real ? C\'est déjà fait!',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore error distinctio doloremque inventore eligendi atque ad dolor pariatur illum eos, nemo perferendis quos vitae alias modi porro assumenda tempora rerum!'
     }
 ];
 
 // EVENT LISTENERS
-btnSearch.addEventListener('click', () => {
-    displayResult();
-});
 
-inputSearch.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
+btnNavHome.addEventListener('click', () => {
+    app.innerHTML = '';
+    app.insertAdjacentHTML('afterbegin', homeMarkup);
+})
+
+
+btnNavSearch.addEventListener('click', () => {
+    app.innerHTML = '';
+    app.insertAdjacentHTML('afterbegin', createSearchBar())
+    inputSearch = document.getElementById('input--search');
+    btnSearch = document.getElementById('btn--search');
+    cardResult = document.getElementById('card--result');
+
+    btnSearch.addEventListener('click', () => {
         displayResult();
+        btnArticleDelete = document.getElementById('btn--article--delete');
+    });
 
-    }
+    inputSearch.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            displayResult();
+        }
+    });
 });
+btnNavCreateArticle.addEventListener('click', () => {
+    app.innerHTML = '';
+    app.insertAdjacentHTML('afterbegin', createArticleMarkup);
+    btnCreateArticle = document.getElementById('btn--create-article');
+    inputCreateArticleTitle = document.getElementById('input--create-article--title');
+    inputCreateArticleDescription = document.getElementById('input--create-article--description');
+
+    btnCreateArticle.addEventListener('click' , () => {
+        const newArticle = {
+            id: +Date.now().toString(),
+            title: inputCreateArticleTitle.value,
+            description: inputCreateArticleDescription.value
+        }
+        articles.push(newArticle);
+        inputCreateArticleDescription.value = '';
+        inputCreateArticleTitle.value = '';
+
+    })
+
+});
+
 
 // FUNCTIONS
 function filter(word) {
@@ -46,35 +94,91 @@ function filter(word) {
 ]
 */
 
-function createMarkup(article) {
+function createSearchBar() {
     return `
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${article.title}</h5>
-      <p class="card-text">${article.description}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="row mb-2">
+        <div class="col-6">
+            <input class="form-control" id="input--search" placeholder="Type your research" type="text">
+        </div>
+        <div class="col-4">
+            <button class="btn btn-primary" id="btn--search" type="button">Search</button>
+        </div>
     </div>
-  </div>
+    <div class="row" id="card--result"></div>
     `;
+}
+
+function createResultCard(article) {
+    return `
+    <div class="col-6">
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${article.title}</h5>
+                <p class="card-text">${article.description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-danger" id="${article.id}">Delete</a>
+            </div>
+        </div>
+    </div>
+`;
 }
 
 function displayResult() {
     // Permet de supprimer tout le contenu de la balise html
     // Avant l'insertion du markup.
-    app.innerHTML = '';
+    cardResult.innerHTML = '';
     // trim() permet d'enlever les espaces au début et à la fin d'une string.
     const word = inputSearch.value.toUpperCase().trim();
     const foundArticles = filter(word);
-    console.log(foundArticles);
     if (word && foundArticles.length > 0) {
         foundArticles.forEach(article => {
-            const _markup = createMarkup(article);
-            app.insertAdjacentHTML('afterbegin', _markup);
+            const _markup = createResultCard(article);
+            cardResult.insertAdjacentHTML('beforeend', _markup);
         });
     } else if (word && foundArticles.length === 0) {
-        app.insertAdjacentHTML('afterbegin', `<p>Aucun résultat.</p>`);
+        cardResult.insertAdjacentHTML('beforeend', `<p>Aucun résultat.</p>`);
     } else {
-        app.insertAdjacentHTML('afterbegin', `<p class="text-danger">Veuillez taper au moins 1 mot.</p>`);
+        cardResult.insertAdjacentHTML('beforeend', `<p class="text-danger">Veuillez taper au moins 1 mot.</p>`);
     }
 }
+
+const homeMarkup = `
+    <div class="row">
+    <div class="col">
+    <h3>Bienvenue sur notre application de journalistes du dimanche.</h3>
+</div>
+</div>
+`
+
+const createArticleMarkup = `
+    <div class="row">
+        <div class="col">
+            <h4 class="d-flex justify-content-center">Create a new article</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="input--create-article--title" placeholder="Type your title">
+                <label for="floatingInput">Title</label>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Description" id="input--create-article--description" style="height: 100px"></textarea>
+                <label for="floatingTextarea2">Description</label>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-6">
+            <button class="btn btn-primary" type="button" id="btn--create-article">Create article</button>
+        </div>
+    </div>
+    
+`;
+
+app.insertAdjacentHTML('afterbegin', homeMarkup);
